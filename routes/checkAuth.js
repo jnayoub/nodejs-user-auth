5147f30371a4769247b301456ca2a03a91d6a441
Joel Ayoub <jnayoub@gmail.com>
2023-10-29 18:42:40 -0400

    update auth

--- routes/checkAuth.js
+++ routes/checkAuth.js
@@ -13,18 +13,28 @@ userRouter.use(cookieParser());
 
 
 userRouter.use(
-  cors({
-    origin: "*",
-    credentials: true
-  })
-);
+    cors({
+      origin: "https://azure-storage-account-d446090db141.herokuapp.com",  // Replace with the origin of App2
+      credentials: true  // This allows the server to accept cookies sent from the origin
+    })
+  );
 
 userRouter.get('/check-auth', (req, res) => {
     console.log('Request Headers:', req.headers);
     console.log('Cookies:', req.cookies);
     console.log('user hit check auth v2')
-    const token = req.cookies.jwt;  // Assuming the cookie's name is "jwt"
-
+    let token;
+    
+    // Check if token is in cookies first
+    if (req.cookies && req.cookies.jwt) {
+        token = req.cookies.jwt;
+    } else {
+        // If not in cookies, check the Authorization header
+        const bearerHeader = req.headers['authorization'];
+        if (bearerHeader && bearerHeader.startsWith('Bearer ')) {
+            token = bearerHeader.split(' ')[1];
+        }
+    }
     if (!token) {
         return res.status(401).json({ message: "No token found." });
     }
